16ad61502ad2f3eb85531f8cd53bd9c830f5deb2
Seiji Noda <m230921@dac.unicamp.br>
2023-06-20 16:45:12 -0300

    add enemies sprites and draw logic

--- src/gameobjects/enemy/BlueEnemy.java
+++ src/gameobjects/enemy/BlueEnemy.java
@@ -2,22 +2,44 @@ package gameobjects.enemy;
 
 import java.awt.Color;
 import java.awt.Graphics;
+import java.awt.Image;
+
+import javax.swing.ImageIcon;
 
 import directions.DirectionsEnum;
 
 public class BlueEnemy extends Enemy{
-	private static int SPEED = 32;
+	private static int SPEED = 16;
 	private static int HEIGHT = 32;
 	private static int WIDTH = 32;
 	
 	private DirectionsEnum direction = DirectionsEnum.EAST;
 	
-	private Color color = Color.BLUE;
+	private Image[] imagesRight = new Image[2];
+	private Image[] imagesLeft = new Image[2];
+	private int frame = 0;
+	
+	private void loadImages(String imagesDirectory) {
+		ImageIcon ii;
+		System.out.println(imagesDirectory);
+		// sprite for when enemy is facing East
+		ii = new ImageIcon(imagesDirectory + "blue_0.png");
+		this.imagesRight[0] = ii.getImage();
+		ii = new ImageIcon(imagesDirectory + "blue_1.png");
+		this.imagesRight[1] = ii.getImage();
+		
+		// sprite for when enemy is facing West
+		ii = new ImageIcon(imagesDirectory + "blue_2.png");
+		this.imagesLeft[0] = ii.getImage();
+		ii = new ImageIcon(imagesDirectory + "blue_3.png");
+		this.imagesLeft[1] = ii.getImage();
+	}
 	
 	public BlueEnemy() {
 		this.setSpeed(SPEED);
 		this.setHeight(HEIGHT);
 		this.setWidth(WIDTH);
+		loadImages("src/images/enemies/");
 	}
 	
 	public BlueEnemy(int x, int y) {
@@ -26,13 +48,13 @@ public class BlueEnemy extends Enemy{
 		this.setWidth(WIDTH);
 		this.setX(x);
 		this.setY(y);
+		loadImages("src/images/enemies/");
 	}
 	
 	public void draw(Graphics g) {
-		Color originalGraphicsColor = g.getColor();
-		g.setColor(this.color);
-		g.fillRect(this.getX(), this.getY(), this.getWidth(), this.getHeight());
-		g.setColor(originalGraphicsColor);
+		Image image = this.direction == DirectionsEnum.EAST ? imagesRight[frame++] : imagesLeft[frame++];
+		if (frame == 2) frame = 0;
+		g.drawImage(image, this.getX(), this.getY(), null);
 	}
 	
 	public void move() {
